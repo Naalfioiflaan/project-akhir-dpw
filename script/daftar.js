9108dd91ee78cc8b7fcd2e2e1fccc1ebe3032258
Nadira Alifia <alifianadira11@gmail.com>
2022-06-04 09:41:24 +0700

    fix kan mobile daftar

--- script/daftar.js
+++ script/daftar.js
@@ -15,3 +15,21 @@ function myFunction2() {
       x.type = "password";
     }
 }
+
+function myFunction3() {
+  var x = document.getElementById("password_m");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("konfirmasi_m");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
